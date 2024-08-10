09892243fc6f53b14e107fe1a9bbc9947c145828
mahesh <maheshbabu.sesetty@chromiumsolutions.com>
2024-08-10 23:53:15 +0530

    basic datatypes, let,const and function

---
+++ app.js
@@ -0,0 +1,36 @@
+const name = 'Mahesh';
+let age = 29;
+const hobbies = true;
+
+age = 28;
+
+
+const userSummarize = (userName, userAge, userHobbies) => userName + userAge + userHobbies;
+
+const randamOne = () => 1+1;
+
+
+//flutter
+//obx(){return ;}
+//obx() =>
+//setState((){});
+//setState(()=>)
+//onTap:(){}
+
+
+
+
+function summarize(userName, userAge, userHobbies) {
+    return (
+        userName + userAge + userHobbies
+    );
+}
+
+//console.log(summarize(name, age, hobbies));
+console.log(userSummarize(name,age,hobbies));
+console.log(randamOne());
+
+
+
+
+
