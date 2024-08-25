43df3c35b8865b4ab915396af47bcd7e32c5096c
mahesh <maheshbabu.sesetty@chromiumsolutions.com>
2024-08-25 19:19:28 +0530

    async

--- app.js
+++ app.js
@@ -1,2 +1,40 @@
 
 
+// setInterval(() => {
+//     console.log("Hello World");
+
+// }, 2000);
+
+const fs = require('fs');
+
+var names = "Maheshs";
+
+
+const asyncFuction = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (names == "Mahesh") {
+            resolve("Success!")
+        } else {
+            reject("Failed");
+        }
+
+    }, 2000);
+});
+
+asyncFuction.then((message) => {
+    console.log(message);
+}).catch((err) => {
+    console.log(err);
+});
+
+
+const readAndWriteFile = (filePath) => {
+    const promiseFile = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            fs.readFile()
+        }, 250);
+    })
+}
+
+
+
